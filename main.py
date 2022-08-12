6149639481d48238a9e0e796d11ffb79b7aa4c7d
rafarbop <67751655+rafarbop@users.noreply.github.com>
2022-08-12 19:43:40 -0300

    test 3 - new timezone info in conversion

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 from decouple import config
 from datetime import datetime
 import time
+import pytz
 
 API_KEY = config("TOKEN_TELEGRAM")
 
@@ -10,9 +11,10 @@ bot = telebot.TeleBot(API_KEY)
 @bot.message_handler(func=lambda message: True)
 def responder(msg):
     # To send a Message File
+    timezone_local = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
     info = f'''
     Unix Timestemp: {msg.date}
-    Time: {datetime.fromtimestamp(msg.date,tz=time.tzname[0])}
+    Time: {datetime.fromtimestamp(msg.date,timezone_local)}
     Timezone: {time.tzname}
     '''
     bot.reply_to(
